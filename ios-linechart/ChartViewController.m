6a16623f9e79220d5a083d9074e3b67e440f2541
Marcel Ruegenberg <dustlab@dustlab.com>
2014-03-22 21:36:55 +0100

    Tuned.

--- ios-linechart/ChartViewController.m
+++ ios-linechart/ChartViewController.m
@@ -28,83 +28,89 @@
         [self.formatter setDateFormat:[NSDateFormatter dateFormatFromTemplate:@"yyyyMMMd" options:0 locale:[NSLocale currentLocale]]];
     }
     
-    LCLineChartData *d1x = [LCLineChartData new];
+    // first sample chart view:
     {
-        LCLineChartData *d1 = d1x;
-        // el-cheapo next/prev day. Don't use this in your Real Code (use NSDateComponents or objc-utils instead)
-        NSDate *date1 = [[NSDate date] dateByAddingTimeInterval:((-3) * SECS_PER_DAY)];
-        NSDate *date2 = [[NSDate date] dateByAddingTimeInterval:((2) * SECS_PER_DAY)];
-        d1.xMin = [date1 timeIntervalSinceReferenceDate];
-        d1.xMax = [date2 timeIntervalSinceReferenceDate];
-        d1.title = @"Foobarbang";
-        d1.color = [UIColor redColor];
-        d1.itemCount = 6;
-        NSMutableArray *arr = [NSMutableArray array];
-        for(NSUInteger i = 0; i < 4; ++i) {
-            [arr addObject:@(d1.xMin + (rand() / (float)RAND_MAX) * (d1.xMax - d1.xMin))];
-        }
-        [arr addObject:@(d1.xMin)];
-        [arr addObject:@(d1.xMax)];
-        [arr sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
-            return [obj1 compare:obj2];
-        }];
-        NSMutableArray *arr2 = [NSMutableArray array];
-        for(NSUInteger i = 0; i < 6; ++i) {
-            [arr2 addObject:@((rand() / (float)RAND_MAX) * 6)];
-        }
-        d1.getData = ^(NSUInteger item) {
-            float x = [arr[item] floatValue];
-            float y = [arr2[item] floatValue];
-            NSString *label1 = [self.formatter stringFromDate:[date1 dateByAddingTimeInterval:x]];
-            NSString *label2 = [NSString stringWithFormat:@"%f", y];
-            return [LCLineChartDataItem dataItemWithX:x y:y xLabel:label1 dataLabel:label2];
-        };
+        LCLineChartData *d1x = ({
+            LCLineChartData *d1 = [LCLineChartData new];
+            // el-cheapo next/prev day. Don't use this in your Real Code (use NSDateComponents or objc-utils instead)
+            NSDate *date1 = [[NSDate date] dateByAddingTimeInterval:((-3) * SECS_PER_DAY)];
+            NSDate *date2 = [[NSDate date] dateByAddingTimeInterval:((2) * SECS_PER_DAY)];
+            d1.xMin = [date1 timeIntervalSinceReferenceDate];
+            d1.xMax = [date2 timeIntervalSinceReferenceDate];
+            d1.title = @"Foobarbang";
+            d1.color = [UIColor redColor];
+            d1.itemCount = 6;
+            NSMutableArray *arr = [NSMutableArray array];
+            for(NSUInteger i = 0; i < 4; ++i) {
+                [arr addObject:@(d1.xMin + (rand() / (float)RAND_MAX) * (d1.xMax - d1.xMin))];
+            }
+            [arr addObject:@(d1.xMin)];
+            [arr addObject:@(d1.xMax)];
+            [arr sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+                return [obj1 compare:obj2];
+            }];
+            NSMutableArray *arr2 = [NSMutableArray array];
+            for(NSUInteger i = 0; i < 6; ++i) {
+                [arr2 addObject:@((rand() / (float)RAND_MAX) * 6)];
+            }
+            d1.getData = ^(NSUInteger item) {
+                float x = [arr[item] floatValue];
+                float y = [arr2[item] floatValue];
+                NSString *label1 = [self.formatter stringFromDate:[date1 dateByAddingTimeInterval:x]];
+                NSString *label2 = [NSString stringWithFormat:@"%f", y];
+                return [LCLineChartDataItem dataItemWithX:x y:y xLabel:label1 dataLabel:label2];
+            };
+            
+            d1;
+        });
+        
+        LCLineChartData *d2x = ({
+            LCLineChartData *d1 = [LCLineChartData new];
+            NSDate *date1 = [[NSDate date] dateByAddingTimeInterval:((-3) * SECS_PER_DAY)];
+            NSDate *date2 = [[NSDate date] dateByAddingTimeInterval:((2) * SECS_PER_DAY)];
+            d1.xMin = [date1 timeIntervalSinceReferenceDate];
+            d1.xMax = [date2 timeIntervalSinceReferenceDate];
+            d1.title = @"Bar";
+            d1.color = [UIColor blueColor];
+            d1.itemCount = 8;
+            NSMutableArray *arr = [NSMutableArray array];
+            for(NSUInteger i = 0; i < d1.itemCount - 2; ++i) {
+                [arr addObject:@(d1.xMin + (rand() / (float)RAND_MAX) * (d1.xMax - d1.xMin))];
+            }
+            [arr addObject:@(d1.xMin)];
+            [arr addObject:@(d1.xMax)];
+            [arr sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+                return [obj1 compare:obj2];
+            }];
+            NSMutableArray *arr2 = [NSMutableArray array];
+            for(NSUInteger i = 0; i < d1.itemCount; ++i) {
+                [arr2 addObject:@((rand() / (float)RAND_MAX) * 6)];
+            }
+            d1.getData = ^(NSUInteger item) {
+                float x = [arr[item] floatValue];
+                float y = [arr2[item] floatValue];
+                NSString *label1 = [self.formatter stringFromDate:[date1 dateByAddingTimeInterval:x]];
+                NSString *label2 = [NSString stringWithFormat:@"%f", y];
+                return [LCLineChartDataItem dataItemWithX:x y:y xLabel:label1 dataLabel:label2];
+            };
+            
+            d1;
+        });
+        
+        LCLineChartView *chartView = [[LCLineChartView alloc] initWithFrame:CGRectMake(20, 400, 500, 300)];
+        chartView.yMin = 0;
+        chartView.yMax = 6;
+        chartView.ySteps = @[@"1.0",@"2.0",@"3.0",@"4.0",@"5.0",@"A big label at 6.0"];
+        chartView.data = @[d1x,d2x];
+        
+        //    chartView.drawsDataPoints = NO; // Uncomment to turn off circles at data points.
+        //    chartView.drawsDataLines = NO; // Uncomment to turn off lines connecting data points.
+        //    chartView.backgroundColor = [UIColor colorWithWhite:0.95 alpha:1.0]; // Uncomment for custom background color.
+        
+        [self.view addSubview:chartView];
     }
     
-    LCLineChartData *d2x = [LCLineChartData new];
-    {
-        LCLineChartData *d1 = d2x;
-        NSDate *date1 = [[NSDate date] dateByAddingTimeInterval:((-3) * SECS_PER_DAY)];
-        NSDate *date2 = [[NSDate date] dateByAddingTimeInterval:((2) * SECS_PER_DAY)];
-        d1.xMin = [date1 timeIntervalSinceReferenceDate];
-        d1.xMax = [date2 timeIntervalSinceReferenceDate];
-        d1.title = @"Bar";
-        d1.color = [UIColor blueColor];
-        d1.itemCount = 8;
-        NSMutableArray *arr = [NSMutableArray array];
-        for(NSUInteger i = 0; i < d1.itemCount - 2; ++i) {
-            [arr addObject:@(d1.xMin + (rand() / (float)RAND_MAX) * (d1.xMax - d1.xMin))];
-        }
-        [arr addObject:@(d1.xMin)];
-        [arr addObject:@(d1.xMax)];
-        [arr sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
-            return [obj1 compare:obj2];
-        }];
-        NSMutableArray *arr2 = [NSMutableArray array];
-        for(NSUInteger i = 0; i < d1.itemCount; ++i) {
-            [arr2 addObject:@((rand() / (float)RAND_MAX) * 6)];
-        }
-        d1.getData = ^(NSUInteger item) {
-            float x = [arr[item] floatValue];
-            float y = [arr2[item] floatValue];
-            NSString *label1 = [self.formatter stringFromDate:[date1 dateByAddingTimeInterval:x]];
-            NSString *label2 = [NSString stringWithFormat:@"%f", y];
-            return [LCLineChartDataItem dataItemWithX:x y:y xLabel:label1 dataLabel:label2];
-        };
-    }
-    
-    LCLineChartView *chartView = [[LCLineChartView alloc] initWithFrame:CGRectMake(20, 400, 500, 300)];
-    chartView.yMin = 0;
-    chartView.yMax = 6;
-    chartView.ySteps = @[@"1.0",@"2.0",@"3.0",@"4.0",@"5.0",@"A big label at 6.0"];
-    chartView.data = @[d1x,d2x];
-
-//    chartView.drawsDataPoints = NO; // Uncomment to turn off circles at data points.
-//    chartView.drawsDataLines = NO; // Uncomment to turn off lines connecting data points.
-//    chartView.backgroundColor = [UIColor colorWithWhite:0.95 alpha:1.0]; // Uncomment for custom background color.
-
-    [self.view addSubview:chartView];
-    
+    // second sample chart view
     {
         LCLineChartData *d = [LCLineChartData new];
         d.xMin = 1;
